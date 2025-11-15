b6fb930922fade9ad96d1807d6cfa45172a70584
Roman <sityukroman44@gmail.com>
2025-11-16 01:50:03 +0200

    review hw2

--- src/main/java/skillklan/module1/theme4/Homework2ArrayNames.java
+++ src/main/java/skillklan/module1/theme4/Homework2ArrayNames.java
@@ -2,8 +2,10 @@ package skillklan.module1.theme4;
 
 public class Homework2ArrayNames {
     public static void main(String[] args) {
-//        String[] arr = {"Борис, Іван, Руслан, Ірина, Олена"};
+//        String[] arr = {"Борис, Іван, Руслан, Ірина, Олена"};//TODO якщо тут зрозумів в чому помилка, то видали цей коментар він тут не потрібен
         String[] arr = {"Борис", "Іван", "Руслан", "Ірина", "Олена"};
+        // TODO: Цикл for написаний правильно, але краще використовувати for-each для читабельності.
+        // TODO: Замість arr[n] краще використати for (String name : arr)
         for (int n = 0; n < arr.length; n++) {
             System.out.println(arr[n]);
         }
