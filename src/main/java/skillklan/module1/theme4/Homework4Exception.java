01725ee6b8161094831b7dc362d3a3bb0d56b2b2
Roman <sityukroman44@gmail.com>
2025-11-16 16:04:38 +0200

    review hw4

--- src/main/java/skillklan/module1/theme4/Homework4Exception.java
+++ src/main/java/skillklan/module1/theme4/Homework4Exception.java
@@ -9,16 +9,24 @@ public class Homework4Exception {
         int a = in.nextInt();
         System.out.print("Введіть дільник: ");
         int b = in.nextInt();
-        zero(a, b);
+        zero(a, b);// TODO: Виклик методу zero — ок, але добре передавати результат у змінну
         System.out.println("Кінець программи");
     }
 
     static void zero(int a, int b) {
         try {
-            int result = a / b;
+            int result = a / b;// TODO: Цей код правильний, але краще не створювати змінну result — можна одразу виводити
             System.out.println("Результат: " + result);
         } catch (Exception с) {
+            // TODO: Ловити Exception — занадто загально.
+            // маємо ловити саме ArithmeticException — так правильно!
             System.out.println("На нуль ділити поки що не можна");
         }
+
+        // TODO: Додати finally — щоб попрактикувати завершальні блоки.
+        // Наприклад:
+        // finally {
+        //     System.out.println("Обробка завершена");
+        // }
     }
 }
