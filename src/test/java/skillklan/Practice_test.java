e1cf62a6839e4aafdbc062a84e97d745333459e6
SorokinGreg <mrgregory1993@gmail.com>
2025-12-19 13:15:35 +0100

    АFixed

--- src/test/java/skillklan/Practice_test.java
+++
@@ -1,9 +0,0 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-public class Practic {
-    public static void main(String[] args) {
-        WebDriver chrome = new WebDriver();
-        chrome.get("");
-    }
-}
